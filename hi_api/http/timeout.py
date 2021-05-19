b9542db5ac6065558257ff5c9a395f3b98392242
chenlei192 <criss.chan@foxmail.com>
2021-05-19 10:10:00 +0800

    add hi_api/http/retries,timeout classes

---
+++ hi_api/http/timeout.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/19 10:02 上午
+# @Author  : CrissChan
+# @Site    : https://blog.csdn.net/crisschan
+# @File    : timeout.py
+# @Software: HTTP访问请求等待时间，等同于思考时间
+
+import requests
+from requests.adapters import HTTPAdapter
+DEFAULT_TIMEOUT = 5 # 默认等待时间，也就是思考时间，单位seconds
+
+class TimeoutHTTPAdapter(HTTPAdapter):
+    def __init__(self, *args, **kwargs):
+        self.timeout = DEFAULT_TIMEOUT
+        if "timeout" in kwargs:
+            self.timeout = kwargs["timeout"]
+            del kwargs["timeout"]
+        super().__init__(*args, **kwargs)
+
+    def send(self, request, **kwargs):
+        timeout = kwargs.get("timeout")
+        if timeout is None:
+            kwargs["timeout"] = self.timeout
+        return super().send(request, **kwargs)
+
+
+# if __name__ == '__main__':
+#     http = requests.Session()
+#
+#     # 把配置复制给http和https请求
+#     adapter = TimeoutHTTPAdapter(timeout=2.5)
+#     http.mount("https://", adapter)
+#     http.mount("http://", adapter)
+#
+#     # 使用全局等待时间2.5秒
+#     response = http.get("https://blog.csdn.net/fadfadfadf")
+#     # 不用全局等待时间，设置请求自己的等待时间10秒
+#     response = http.get("https://blog.csdn.net/crisschan", timeout=10)
+

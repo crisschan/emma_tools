a158003411ad22f1ef7d3939d2b84c83e2ce3f42
chenlei192 <criss.chan@foxmail.com>
2021-06-29 23:52:52 +0800

    add httper

--- hi_api/httper/__init__.py
+++ hi_api/httper/__init__.py
@@ -5,24 +5,25 @@
 # @Site    : https://blog.csdn.net/crisschan
 # @File    : __init__.py.py
 # @Software: PyCharm
+import requests
 from retries import ReTriesHTTPAdapter
-from config_ini import ConfigFileParser
 from requests_toolbelt import sessions
-from config_ini import ConfigFileParser
-
-
+from ..CONFIG import BASE_URL
 class httper(sessions.BaseUrlSession):
-    def __init__(self,base_url=None):
-        cf = ConfigFileParser('../config.ini')
-        self.base_url=cf.get_value('baseurl','baseurl')
-        super().__init__()
+    def __init__(self,base_url=BASE_URL):
+        self.base_url=base_url
+        super(httper, self).__init__(base_url=base_url)
         self.http_retries()
-
-
     def http_retries(self):
-        readapter = ReTriesHTTPAdapter(max_retries=3)
+        readapter = ReTriesHTTPAdapter()
         return super().mount("http://",readapter)
 
 
 
-
+# if __name__ == '__main__':
+#     cf = ConfigFileParser('../config.ini')
+#     base_url = cf.get_value('baseurl', 'baseurl')
+#
+#     h =httper(base_url=base_url)
+#     a = h.get('/category_1404515.html')
+#     print(a.headers)
--- hi_api/httper/retries.py
+++ hi_api/httper/retries.py
@@ -7,32 +7,11 @@
 # @Software: HTTP访问失败后的重试次数
 import requests
 from requests.adapters import HTTPAdapter
-from config_ini import ConfigFileParser
+from ..CONFIG import MAX_RETRIES
 
 class ReTriesHTTPAdapter(HTTPAdapter):
     def __init__(self,*arg,**kwargs):
         if "max_retries" not in kwargs:
-            cf = ConfigFileParser(file='../config.ini')
-            try:
-                kwargs["max_retries"]= cf.get_value('defaultmaxretries', 'defaultmaxretries')
-            except:
-                kwargs["max_retries"]=1
+            kwargs["max_retries"]= MAX_RETRIES
         super().__init__(*arg,**kwargs)
 
-
-
-if __name__ == '__main__':
-
-    http = requests.Session()
-#
-#     # 把配置复制给http和https请求
-#     readapter  = ReTriesHTTPAdapter(max_retries=3)
-#     http.mount("http://",readapter)
-#
-#     # 使用全局等待时间2.5秒
-#     response = http.get("https://blog.csdn.net/fadfadfadf")
-#     print(response.text)
-#     # 不用全局等待时间，设置请求自己的等待时间10秒
-#     response = http.get("https://blog.csdn.net/crisschan", timeout=10)
-#
-#     print(response.text)

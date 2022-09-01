f07711254ab3d60036a33450d9dc85f69995c288
crisschan <can101208@gmail.com>
2022-09-01 17:47:29 +0800

    Update search_page.py
    modi the wrong code

--- hi_po_Exp/pages/search_page.py
+++ hi_po_Exp/pages/search_page.py
@@ -12,12 +12,4 @@ class SearchPage(PageObject):
 
 
 
-class PlayPage(PageObject):
-    '''课程播放页'''
-    search_buttom = PageElement(xpath='//*[@id="search"]/div/div[2]/button/i')
-    user_name_txt = PageElement(xpath='//*[@id="key"]')
-    password_txt = PageElement(xpath='//*[@id="search"]/div/div[2]/button/i')
-    login_but = PageElement(xpath='//*[@id="J_feature"]/ul/li[1]/a/i')
-    search_box = PageElement(xpath='//*[@id="key"]')
-    search_result = PageElement(xpath='//*[@id="J_goodsList"]/ul/li[1]/div/div[3]/a/em/font')
-    search_checkbox = PageElement(xpath='//*[@id="J_feature"]/ul/li[1]/a/i')
+
